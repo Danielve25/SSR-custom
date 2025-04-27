7e5ee90c4e98e5f5ab744f3fe4965da0918c44a0
danielve25 <danielvelascocuentaestudiantil@gmail.com>
2025-04-27 14:09:46 -0500

    añadir streamin ssr

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
-import { renderToString } from 'react-dom/server'
+import { renderToPipeableStream } from 'react-dom/server'
 import App from './App'
 
 /**
  * @param {string} _url
+ * @param {import('react-dom/server').RenderToPipeableStreamOptions} [options]
  */
-export function render(_url) {
-  const html = renderToString(
+export function render(_url, options) {
+  return renderToPipeableStream(
     <StrictMode>
       <App />
     </StrictMode>,
+    options,
   )
-  return { html }
 }
